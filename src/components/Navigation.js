865acd1f7573a2d233d6e6bdd5334155308b45f9
dypool01 <dypool01@wsc.edu>
2022-12-21 01:24:16 -0600

    App created, making the resume page

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+export default function Navigation() {
+    return (
+        <Navbar expand='md' variant='dark' bg='dark' className='p-3'>
+            <Navbar.Brand href='/'>Dylan Poole</Navbar.Brand>
+
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+                <Nav>
+                    {/* LINKS GO IN HERE */}
+                
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
