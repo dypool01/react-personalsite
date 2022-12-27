89093a891d1da50b8bd2b226ee31122d1ca50314
dypool01 <dypool01@wsc.edu>
2022-12-27 06:10:17 -0600

    Made a functional contact form, and styled the content so its stacked and in the middle of the page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,6 +3,10 @@ import { ContactUs } from "./ContactForm";
 
 export default function Contact() {
     return (
+        <>
+        <h1>Contact Me</h1>
+
         <ContactUs />
+        </>
     )
 }
